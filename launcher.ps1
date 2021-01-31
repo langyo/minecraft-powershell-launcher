dc0012757e2eb89464a48d6b2fc6955f346962e8
langyo <langyo.china@gmail.com>
2021-02-01 02:00:31 +0800

    :zap: A server and a client together!

--- launcher.ps1
+++ launcher.ps1
@@ -1,24 +1,47 @@
 Add-Type -Name Window -Namespace Console -MemberDefinition '
 [DllImport("Kernel32.dll")]
 public static extern IntPtr GetConsoleWindow();
-
 [DllImport("user32.dll")]
 public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
 '
 $consolePtr = [Console.Window]::GetConsoleWindow()
 [Console.Window]::ShowWindow($consolePtr, 0)
 
-$path = $env:tmp + '\minecraft-launcher-bootstrap.html'
-'
+
+$serverScriptCode = {
+  param($server)
+  $server.Start()
+  while ($server.IsListening) {
+    $context = $server.GetContext()
+    # $request = $context.Request
+    $response = $context.Response
+
+    $str = Get-Date -Format 'hhmmss'
+    $buffer = [System.Text.Encoding]::UTF8.GetBytes($str)
+    $response.ContentLength64 = $buffer.Length
+    $response.OutputStream.Write($buffer, 0, $buffer.Length)
+    $response.OutputStream.Close()
+  }
+  $server.Stop()
+}
+
+$server = New-Object System.Net.HttpListener
+$server.Prefixes.Add('http://*:9233/')
+$server.AuthenticationSchemes = 'Negotiate'
+
+$serverProcess = [PowerShell]::Create().AddScript($serverScriptCode).AddArgument($server)
+$serverProcess.BeginInvoke()
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$page = '
 <html>
 <body>
 <h1>Bootstrap is writing</h1>
 </body>
 </html>
-' > $path
-
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
+'
 
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Minecraft Launcher'
@@ -28,19 +51,21 @@ $form.ShowIcon = $False
 $form.MinimizeBox = $False
 $form.MaximizeBox = $False
 $form.FormBorderStyle = 'Fixed3D'
-
+  
 $browserBox = New-Object System.Windows.Forms.WebBrowser
 $browserBox.Location = New-Object System.Drawing.Point(0, 0)
 $browserBox.Size = New-Object System.Drawing.Size(600, 400)
 $browserBox.AllowWebBrowserDrop = $False
 $browserBox.IsWebBrowserContextMenuEnabled = $False
 $browserBox.WebBrowserShortcutsEnabled = $False
-$browserBox.Url = $path
+$browserBox.ScriptErrorsSuppressed = $False
+$browserBox.DocumentText = $page
+  
 $form.Controls.Add($browserBox)
-
 $form.Topmost = $true
-
 $form.Add_Shown( { $browserBox.Select() })
 $form.ShowDialog()
-
-Del $path
+$serverEndCode = {
+  $serverProcess.EndEnvoke()
+}
+$form.Add_Closed($serverEndCode)
